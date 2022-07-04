ca02bb980dcca472c8e082eb7a8178db4db32d69
arielmirra <amirra@mulesoft.com>
2022-07-04 00:44:43 -0300

    chore: move js files to tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
-import type { NextPage } from 'next'
 import * as React from 'react';
+import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
 import Link from '../src/Link';
+import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          MUI v5 + Next.js with TypeScript example
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
@@ -21,7 +29,7 @@ const Home: NextPage = () => {
         <Copyright />
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
