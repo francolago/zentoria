f3125c8a9b6a7c06ab783da51657982642c2da64
hector997 <hectorvillariino@gmail.com>
2023-07-09 15:49:41 -0300

    update: hero texts & li

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -23,12 +23,19 @@ export default function Hero() {
           unipersonales, sociedades,
           monotributistas, asociaciones sin fines de lucro, fundaciones, fideicomisos y constructoras.
         </Description>
+        <Subheading>Consultoría Empresarial </Subheading>
+        <Description>
+          Más de 20 años de experiencia en consultoría, gestión de proyectos, mejora de procesos, habilitaciones, y capacitaciones in company.
+        </Description>
+
+        <ListItem>Project Management | Process Improvement | Habilitaciones</ListItem>
+
         <CustomButtonGroup>
           {/*<Button onClick={() => setIsModalOpened(true)}>*/}
           {/*  Subscribe to the newsletter <span>&rarr;</span>*/}
           {/*</Button>*/}
           <NextLink href="contact" passHref>
-            <Button>
+            <Button >
               Contactanos <span>&rarr;</span>
             </Button>
           </NextLink>
@@ -42,12 +49,12 @@ export default function Hero() {
       <ImageContainer>
         {/*<HeroIllustration />*/}
         <Image src="/landing/sueldos-y-jornales.jpg"
-               width={500}
-               height={500 / ratio}
-               layout="fixed"
-               onLoadingComplete={({ naturalWidth, naturalHeight }) =>
-                 setRatio(naturalWidth / naturalHeight)
-               }
+          width={500}
+          height={500 / ratio}
+          layout="fixed"
+          onLoadingComplete={({ naturalWidth, naturalHeight }) =>
+            setRatio(naturalWidth / naturalHeight)
+          }
         />
       </ImageContainer>
     </HeroWrapper>
@@ -95,7 +102,7 @@ const ImageContainer = styled.div`
 `
 
 const Description = styled.p`
-  font-size: 1.8rem;
+  font-size: 1.6rem;
   opacity: 0.8;
   line-height: 1.6;
 
@@ -104,10 +111,21 @@ const Description = styled.p`
   }
 `
 
+const ListItem = styled.li`
+  margin: 10px 0;
+  font-size: 1.4rem;
+  font-weight: 900;
+  color: rgb(82, 82, 82);
+`
+
 const CustomOverTitle = styled(OverTitle)`
   margin-bottom: 2rem;
 `
-
+const Subheading = styled.h2`
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  font-size: 22px
+`
 const Heading = styled.h1`
   font-size: 7.2rem;
   font-weight: bold;
