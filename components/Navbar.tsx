d5df0c34575832eec4ceafea67485f95da12ba2c
hector997 <hectorvillariino@gmail.com>
2023-07-09 15:12:04 -0300

    update: react fix & logos update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -69,7 +69,7 @@ export default function Navbar({ items }: NavbarProps) {
       <Content>
         <NextLink href="/" passHref>
           <LogoWrapper>
-            <Image alt="logo lorenzo" src="/logo/zentoria.jpg" width={260} height={130} />
+            <Image alt="logo lorenzo" src="/logo/main-logo.gif" width={260} height={130} />
           </LogoWrapper>
         </NextLink>
         <NavItemList>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/logo/favicon-zentoria.gif" />
         {/* <link rel="alternate" type="application/rss+xml" href={EnvVars.URL + 'rss'} title="RSS 2.0" /> */}
         {/* <script
           dangerouslySetInnerHTML={{
